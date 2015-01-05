0c2e8a0e9574f49fb1ab124a34bf213450dc1a0d
46728217 <46728217@sina.com>
2015-01-05 07:12:05 +0000

    c++ thrift test

---
+++ Service/build/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Server.dir/User_types.cpp.o"
+  "CMakeFiles/Server.dir/Server.cpp.o"
+  "CMakeFiles/Server.dir/UserService.cpp.o"
+  "CMakeFiles/Server.dir/User_constants.cpp.o"
+  "Server.pdb"
+  "Server"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
